33c681c1af03e2c8f08bb118f2074cba3b0064e4
root <root@DESKTOP-ANDERSON.localdomain>
2023-08-24 13:28:52 -0300

    first commit

---
+++ excelToBb.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
+
+from dotenv import dotenv_values
+config = dotenv_values(".env")
+print(config['DRIVERNAME'])
+
+url = URL.create(
+    drivername=config['DRIVERNAME'],
+    username=config['USERNAME'],
+    host=config['HOST'],
+    database=config['DATABASE'],
+    password=config['PASSWORD']
+)
+
+engine = create_engine(url)
+
+df = pd.read_excel('teste.xlsx')
+print(df)
+
+with engine.begin() as connection:
+  df.to_sql(name=config['TABLE'], con=connection, if_exists='append', index=False)
